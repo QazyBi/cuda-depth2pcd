de1965722d3705be18a8542f7fcbde47addd909a
Казыбек Аскарбек <qazybi@gmail.com>
2025-03-26 19:01:49 +0300

    removed redundant

--- scripts/run_python.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# run_python.sh: Script to execute the Python orchestration script.
-
-# Run the Python script using python3.
-python3 src/stage1.py
-
-echo "Python orchestration script executed."
